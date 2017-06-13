4eab49def88c6d6cc2148b4de093946d539c5105
Ondoro <ondoros4@gmail.com>
2017-06-13 10:52:45 +0300

    finalised

--- JS/script.js
+++ JS/script.js
@@ -1,24 +1,24 @@
-var pingpong =function(number) {
+ var pingpong = function(number) {
 
-for (var n=1 ;n <= number;n++) {
+   for (var n = 1; n <= number; n++) {
 
-  if (n % 15 === 0) {
-  $("#output").append("<li>" + "pingpong" + "</li>");
-} else if (n % 5 === 0) {
-  $("#output").append("<li>"+ "pong"+ "</li>");
-} else if (n % 3 === 0) {
-$("#output").append("<li>"+"ping"+"</li>");
-} else {
-  $("#output").append("<li>" + n + "</li>")
-}
-}
-};
+     if (n % 15 === 0) {
+       $("#output").append("<li>" + "pingpong" + "</li>");
+     } else if (n % 5 === 0) {
+       $("#output").append("<li>" + "pong" + "</li>");
+     } else if (n % 3 === 0) {
+       $("#output").append("<li>" + "ping" + "</li>");
+     } else {
+       $("#output").append("<li>" + n + "</li>")
+     }
+   }
+ };
 
-$(document).ready(function(){
-  $("form#pingPong").submit(function(event){
-  event.preventDefault();
-  $("#output").text(" ");
-  var number= parseInt($("input#number").val());
-  var result = pingpong(number);
-});
-})
+ $(document).ready(function() {
+   $("form#pingPong").submit(function(event) {
+     event.preventDefault();
+     $("#output").text(" ");
+     var number = parseInt($("input#number").val());
+     var result = pingpong(number);
+   });
+ })
---
+++ README.md
@@ -0,0 +1,34 @@
+# PING-PONG
+
+1.  basic web app.
+
+# About
+
+The styling was accomplished using HTML, CSS and bootstrap while the logic and calculations done using Javascript.
+
+# Specifications
+
+1. can count up to the input number.
+
+
+2. Displays a responsive answer-box showing the output numbers and  corresponding words.
+
+# Contributer
+
+The code was written by [Ondoro](https://github.com/Ondoro)
+
+# Purpose
+
+Moringa School project
+
+# How to use
+
+save the index.html file in your local machine. open the file with your browser.
+
+
+# License
+
+
+The contents of this repository are bounded by the MIT license.
+
+copyright(c) 2017
